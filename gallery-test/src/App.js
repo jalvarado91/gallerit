91f31aa1e553c0be04b02ced26f15a5300426066
Juan Alvarado <jalvarado91@gmail.com>
2018-08-01 20:25:45 -0400

    completes sample gallery layout

--- gallery-test/src/App.js
+++ gallery-test/src/App.js
@@ -1,18 +1,98 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Gallery from "react-photo-gallery";
+import Lightbox from "react-images";
+import Measure from "react-measure";
+import { images } from "./imagesUrls";
+import "./App.css";
+
+const imgSet = images;
 
 class App extends Component {
+  state = {
+    currentImage: 0,
+    lightboxIsOpen: false,
+    width: -1
+  };
+
+  openLightbox = (event, obj) => {
+    this.setState({
+      currentImage: obj.index,
+      lightboxIsOpen: true
+    });
+  };
+
+  closeLightbox = () => {
+    this.setState({
+      currentImage: 0,
+      lightboxIsOpen: false
+    });
+  };
+
+  gotoPrevious = () => {
+    this.setState({
+      currentImage: this.state.currentImage - 1
+    });
+  };
+
+  gotoNext = () => {
+    this.setState({
+      currentImage: this.state.currentImage + 1
+    });
+  };
+
   render() {
+    const { width, currentImage, lightboxIsOpen } = this.state;
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="container">
+          <h1 className="title">Lost and Found</h1>
+          <h2 className="subtitle">
+            The quick brown fox jumped over the lazy fox. What a crazy day!
+          </h2>
+
+          <div className="gallery-wrapper">
+            <Measure
+              bounds
+              onResize={contentRect =>
+                this.setState({ width: contentRect.bounds.width })
+              }
+            >
+              {({ measureRef }) => {
+                if (width < 1) {
+                  return <div ref={measureRef} />;
+                }
+                let columns = 1;
+                if (width >= 480) {
+                  columns = 2;
+                }
+                if (width >= 1024) {
+                  columns = 3;
+                }
+                if (width >= 1824) {
+                  columns = 4;
+                }
+                return (
+                  <div ref={measureRef}>
+                    <Gallery
+                      photos={imgSet}
+                      columns={columns}
+                      onClick={this.openLightbox}
+                    />
+                  </div>
+                );
+              }}
+            </Measure>
+          </div>
+          <Lightbox
+            images={imgSet}
+            onClose={this.closeLightbox}
+            onClickPrev={this.gotoPrevious}
+            onClickNext={this.gotoNext}
+            currentImage={currentImage}
+            isOpen={lightboxIsOpen}
+          />
+        </div>
       </div>
     );
   }
---
+++ gallery-test/src/imagesUrls.js
@@ -0,0 +1,17 @@
+export const images = [
+  { src: "https://i.imgur.com/JsHSISP.jpg", width: 4555, height: 3037  },
+  { src: "https://i.imgur.com/PtiIDOt.jpg", width: 5184, height: 3456 },
+  { src: "https://i.imgur.com/zgsADme.jpg", width: 4555, height: 3037  },
+  { src: "https://i.imgur.com/tRWpLbJ.jpg", width: 4555, height: 3037  },
+  { src: "https://i.imgur.com/fdx9B49.jpg", width: 4555, height: 3037  },
+  { src: "https://i.imgur.com/UXSTQV8.jpg", width: 4555, height: 3037  },
+  { src: "https://i.imgur.com/FOThzAV.jpg", width: 4555, height: 3037  },
+  { src: "https://i.imgur.com/UiNZU1J.jpg", width: 4555, height: 3037  },
+  { src: "https://i.imgur.com/hskaIfn.jpg", width: 2048, height: 1152 },
+  { src: "https://i.imgur.com/5yw2r4B.jpg", width: 4555, height: 3037  },
+  { src: "https://i.imgur.com/Yl6SauV.jpg", width: 4555, height: 3037  },
+  { src: "https://i.imgur.com/YL9sEJ6.jpg", width: 4555, height: 3037  },
+  { src: "https://i.imgur.com/POJsH0t.jpg", width: 4555, height: 3037  },
+  { src: "https://i.imgur.com/jyhALVZ.jpg", width: 4555, height: 3037  },
+  { src: "https://i.imgur.com/6jID1dR.jpg", width: 4555, height: 3037  }
+];
